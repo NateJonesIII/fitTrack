b861dc5bb1a61b5f425872fea5bc9be01486b297
Nate Jones <15nate.jones@gmail.com>
2021-08-28 20:37:51 -0400

    Update to DB, routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,44 +6,36 @@ const db = require("../models/exercise");
 
 // GET Request for getting all workouts
 router.get("/api/workouts", (req, res) => {
-	db.find()
-		.then((dbData) => {
+	db.find().then((dbData) => {
 			res.json(dbData);
-		})
-		.catch((err) => {
+		}).catch((err) => {
 			res.json(err);
 		});
 });
 
 // GET request
 router.get("/api/workouts/range", (req, res) => {
-	db.find()
-		.then((dbData) => {
+	db.find({}).then((dbData) => {
 			res.json(dbData);
-		})
-		.catch((err) => {
+		}).catch((err) => {
 			res.json(err);
 		});
 });
 
 // POST workout
 router.post("/api/workouts", ({ body }, res) => {
-	db.create(body)
-		.then((dbData) => {
+	db.create(body).then((dbData) => {
 			res.json(dbData);
-		})
-		.catch((err) => {
+		}).catch((err) => {
 			res.json(err);
 		});
 });
 
 // PUT/Update workout
 router.put("/api/workouts/:id", ({ body, params }, res) => {
-	db.findByIdAndUpdate(params.id, { $push: { exercises: body } })
-		.then((dbData) => {
+	db.findByIdAndUpdate(params.id, { $push: { exercises: body } }).then((dbData) => {
 			res.json(dbData);
-		})
-		.catch((err) => {
+		}).catch((err) => {
 			res.json(err);
 		});
 });
